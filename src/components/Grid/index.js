8cf1b248cc14fcedec018d00c38c6ce5dea6964a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-08-04 00:18:21 +0500

    add detail

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 // Styles
 import { Wrapper, Content } from "./Grid.styles";
+import PropTypes from "prop-types";
 
 const Grid = ({ header, children }) => (
   <Wrapper>
